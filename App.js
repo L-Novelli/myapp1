472325388683ca70e26291a22dd0a72e56444d19
Lautaro E. Novelli <l.novelli@outlook.com>
2022-07-08 18:20:52 -0300

    update 8/7

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { Button, FlatList, ImageBackground, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {useEffect, useState} from 'react';
 
-import AddItem from './AddITem';
+import AddItem from './components/AddITem';
 import CustomModal from './components/Modal';
-import List from './item list/itemList';
-import React from 'react';
-import {useState} from 'react';
+import List from './components/itemList';
+
+const image = { uri: "https://i.pinimg.com/originals/87/60/50/8760508c95364b7405978967d5936377.jpg" };
 
 export default function App() {
 
@@ -12,41 +13,55 @@ export default function App() {
   const [itemList, setItemList] = useState([]); //useState de array
   const [modalVisible, setModalVisible] = useState(false);
   const [itemSelected, setItemSelected] = useState({});
+  
   const onHandlerChangeItem = (text) => setTextItem(text);
   const onHandlerAddItem = () => { 
-    setItemList(currentItems => [...currentItems, { id: Date.now(), value: textItem}])
+    console.log('se agrego el item'. textItem)
+    setItemList(currentItems => [...currentItems, { id: Date.now(), value: textItem, completed: false}])
     // setItemList({...itemList, id: Math.random()*10, value: textITem}) => hace lo mismo que lo de arriba hehe
     setTextItem('');
   }
   
   const onHandlerDeleteItem = id => {
-      setItemList(currentItems => currentItems.filter(item => item.id !== id))
-      setItemSelected({})
-      setModalVisible(!modalVisible)
+    setItemList(currentItems => currentItems.filter(item => item.id !== id))
+    setItemSelected({})
+    setModalVisible(!modalVisible)
   }
   const onHandlerModal = id => {
-      setItemSelected(itemList.find(item => item.id === id))
-      setModalVisible(!modalVisible)
+    setItemSelected(itemList.find(item => item.id === id))
+    setModalVisible(!modalVisible)
+  }
+  const onHandlerCompleteItem = id => {
+    let itemComleted  = itemList.findIndex(item => item.id === id)
+    itemList[itemComleted].completed = true
+    setItemList({...itemList})
+    setModalVisible(!modalVisible)
+    console.table(itemList)
   }
 
 return (
-    
-    <View style={styles.screen}>
-      <CustomModal
-        modalVisible = {modalVisible}
-        onHandlerDeleteItem = {onHandlerDeleteItem}
-        itemSelected = {itemSelected}
-        />
-      <AddItem
-        textItem = {textItem}
-        onHandlerChangeItem = {onHandlerChangeItem}
-        />
-      <List
-        item  = {itemList}
-        onHandlerModal = {onHandlerModal}
-      />
-      
-    </View>  
+      <ImageBackground  source={image} 
+      resizeMode="cover" 
+      style={styles.image}
+      >
+        <View style={styles.screen}>
+          <CustomModal
+            modalVisible = {modalVisible}
+            onHandlerDeleteItem = {onHandlerDeleteItem}
+            itemSelected = {itemSelected}
+            onHandlerCompleteItem = {onHandlerCompleteItem}
+            />
+          <AddItem
+            textItem = {textItem}
+             onHandlerAddItem = {onHandlerAddItem}
+             onHandlerChangeItem = {onHandlerChangeItem}          
+            />
+ /*         <List
+            item  = {itemList}
+            onHandlerModal = {onHandlerModal}
+          />         
+        </View>  
+      </ImageBackground>
   );
 }
        
@@ -54,5 +69,6 @@ const styles = StyleSheet.create({
   screen: {
     marginTop: '10%',
     padding: 30,
-  },
+    height: '100%',
+  }
 })
--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,43 +1,47 @@
-import { Button, Modal, StyleSheet, Text, View } from "react-native";
+import { Button, Modal, StyleSheet, Text, View } from 'react-native'
 
-export default function CustomModal(props){
-    const{modalVisible, itemSelected, onHandlerDeleteItem} = props
-    return(
+export default function CustomModal(props) {
+
+    const { modalVisible, itemSelected, onHandlerDeleteItem, onHandlerCompleteItem } = props
+
+    return (
         <Modal
-            animationType = "slide"
-            transparent = {true}
-            visible = {modalVisible}
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
         >
             <View style={styles.modal}>
-              <View style={styles.modalView}>
-                <View style={styles.modalTitle}>
-                  <Text>
-                    Mi modal
-                  </Text>
+                <View style={styles.modalView}>
+                    <View style={styles.modalTitle}>
+                        <Text>
+                            Mi modal
+                        </Text>
+                    </View>
+                    <View style={styles.modalMessage}>
+                        <Text>Estas seguro que desea borrar ?</Text>
+                    </View>
+                    <View style={styles.modalMessage}>
+                        <Text style={styles.modalItem}>{itemSelected.value}</Text>
+                    </View>
+                    <View style={styles.modalButton}>
+                        <Button onPress={() => onHandlerDeleteItem(itemSelected.id)} title='Confirmar' />
+                        <Button onPress={() => onHandlerCompleteItem(itemSelected.id)} title='Completar' />
+                    </View>
                 </View>
-                <View style={styles.modalMessage}>
-                  <Text>Estas seguro que desea borrar ?</Text>
-                </View>
-                <View style={styles.modalMessage}>
-                  <Text style={styles.modalItem}>{itemSelected.value}</Text>
-                </View>
-                <View style={styles.modalButton}>
-                  <Button onPress={() => onHandlerDeleteItem(itemSelected.id)} title='Confirmar' />
-                </View>
-              </View>
             </View>
         </Modal>
     )
 }
+
 const styles = StyleSheet.create({
     modal: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'rgba(44,14,55,0.5)'
-      },  
-      modalView: {
-        backgroundColor: 'yellow',
+        backgroundColor: 'rgba(0,0,0,0.5)'
+        },  
+        modalView: {
+        backgroundColor: 'white',
         width: '80%',
         height: '50%',
         borderRadius: 10,
@@ -45,20 +49,24 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'column',
-      },
-      modalTitle: {
-        color: 'black',
-        fontSize: 70
-      },
-      modalMessage: {
+        },
+        modalTitle: {
+        backgroundColor: '#ccc',
+        color: 'white',
+        fontSize: 18,
+        },
+        modalMessage: {
         marginBottom: 10,
         justifyContent: 'center',
         alignItems: 'center'
-      },
-      modalButton: {
+        },
+        modalButton: {
         marginTop: 15,
-      },
-      modalItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+        },
+        modalItem: {
         fontSize: 30
-      }
-    })
+        }
+})
